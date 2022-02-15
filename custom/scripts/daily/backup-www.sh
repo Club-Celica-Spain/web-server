4dc9afa32dc8eb0a6744852dc7f033dd11c8b89b
Xabi <xaabi6@users.noreply.github.com>
2022-02-15 23:39:44 +0100

    Eliminar copias de seguridad antiguas
    Para no llenar todo el espacio disponible del disco con copias de seguridad, se mantendrán nada más que las últimas siete.

--- custom/scripts/daily/backup-www.sh
+++ custom/scripts/daily/backup-www.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Options
 NOW=$(date '+%F')
-FILE=/srv/backup/www/backup-www_${NOW}.tar.gz
+PATH=/srv/backup/www/
+FILE=${PATH}backup-www_${NOW}.tar.gz
+
+# Clean old files before generating a new one
+find ${PATH} -mindepth 1 -mtime +7 -delete
 
 cd /srv/www/
 tar -zcf ${FILE} html
